2df7b700b926ff005bfaf766755f52f9852f7f76
marcoszinga94 <marcoszinga94@gmail.com>
2025-03-15 04:34:45 -0300

    edited pagos

--- src/components/ListaPropiedadesAlquiler.tsx
+++ src/components/ListaPropiedadesAlquiler.tsx
@@ -228,7 +228,7 @@ export default function ListaPropiedadesAlquiler() {
                 </div>
 
                 <div className="flex space-x-2">
-                  <Boton
+                  {/* <Boton
                     onClick={() =>
                       (window.location.href = `/propiedades/${propiedad.id}`)
                     }
@@ -250,7 +250,7 @@ export default function ListaPropiedadesAlquiler() {
                         d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                       />
                     </svg>
-                  </Boton>
+                  </Boton> */}
 
                   <Boton
                     onClick={() => handleEditar(propiedad)}
